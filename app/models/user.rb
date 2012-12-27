8d01faeaf65f24392171066a0780aca56bb33f1b
Andy Phil <philandy01@yahoo.fr>
2012-12-28 05:53:55 +0800

    facebook login

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :firstname, :image, :lastname, :oauth_expires_at, :oauth_token, :phone, :provider, :uid
+end
+class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.firstname = auth.info.first_name
+      user.lastname = auth.info.last_name
+      user.image = auth.info.image
+      user.email = auth.info.email
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121217053146) do
+ActiveRecord::Schema.define(:version => 20121227205513) do
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "image"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
+  end
 
   create_table "wanteds", :force => true do |t|
     t.string   "name"
